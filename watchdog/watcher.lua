bbc50faeda42a1486f01f4737de9ee4e2d7f9cd3
cosinewaves <elkhaadnan@gmail.com>
2025-05-27 23:03:20 +0100

    h

--- watchdog/watcher.lua
+++ watchdog/watcher.lua
@@ -1,9 +1,18 @@
---!strict
-
 local types = require("../watchdog./types")
 
 local watcher = {}
 watcher.__index = watcher
 
+--[=[
+    Watch a given property in an instance, with the ability to stop watching it.
+
+    @param propertyName string
+    @param object Instance
+    @return (() -> (), () -> ())
+]=]
+function watcher.watchProperty(propertyName: string, object: Instance): (() -> (), () -> ())
+    -- TODO here
+    return 
+end
 
 return watcher
