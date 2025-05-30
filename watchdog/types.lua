0ddc28646d162f3d6cc7bcefda9fef07a02d0bf0
cosinewaves <elkhaadnan@gmail.com>
2025-05-30 11:29:13 +0100

    add types

--- watchdog/types.lua
+++ watchdog/types.lua
@@ -1,1 +1,14 @@
+export type DisconnectFunction = () -> ()
+export type ErrorFunction = () -> string?
+
+export type WatchProperty = (propertyName: string, object: Instance) -> (DisconnectFunction, ErrorFunction)
+export type WatchAttribute = (attributeName: string, object: Instance) -> (DisconnectFunction, ErrorFunction)
+export type WatchTag = (tagName: string, callback: (Instance) -> ()) -> (DisconnectFunction, ErrorFunction)
+
+export type Watcher = {
+	watchProperty: WatchProperty,
+	watchAttribute: WatchAttribute,
+	watchTag: WatchTag,
+}
+
 return nil
